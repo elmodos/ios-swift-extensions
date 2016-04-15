ab95b7cddc9611cd755a0c1e6714dd33fd26e123
Modo Ltunzher <offnormal@gmail.com>
2016-04-16 02:37:11 +0300

    UIViewController category for "topViewController"

---
+++ Swift/UIViewController+Helpers.swift
@@ -0,0 +1,58 @@
+//
+//  UIViewController+Helpers.swift
+//  ilostest
+//
+//  Created by Modo Ltunzher on 14.04.16.
+//  Copyright © 2016 Ltunzher. All rights reserved.
+//
+
+// TODO: code id "translated" from obj-c, fix
+
+import UIKit
+
+extension UIViewController // Helpers
+{
+    
+// MARK: - Public
+    
+    class func topViewController() -> UIViewController
+    {
+        return UIViewController.topViewController(UIApplication.sharedApplication().keyWindow!.rootViewController!)
+    }
+    
+    class func topViewController(rootViewController: UIViewController) -> UIViewController
+    {
+        if rootViewController.presentedViewController == nil
+        {
+            return rootViewController
+        }
+        
+        if rootViewController.presentedViewController! is UINavigationController
+        {
+            let navigationController = rootViewController.presentedViewController as! UINavigationController
+            let lastViewController = navigationController.viewControllers.last!
+            return self.topViewController(lastViewController)
+        }
+        
+        let presentedViewController = rootViewController.presentedViewController!
+        return self.topViewController(presentedViewController)
+    }
+    
+    class func navigationController() -> UINavigationController!
+    {
+        let topViewController = self.topViewController()
+        var navigationController: UINavigationController! = nil
+        
+        if topViewController is UINavigationController
+        {
+            navigationController = topViewController as! UINavigationController
+        }
+        else
+        {
+            navigationController = topViewController.navigationController
+        }
+    
+        return navigationController
+    }
+
+}
